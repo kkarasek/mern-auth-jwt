93a2078db3472fe94645f374e37a47bad44ec237
Jakub Karasek <jkarasek@Jakubs-MacBook-Air.local>
2023-07-30 18:58:29 +0200

    add jwt handler

--- backend/controlers/userControler.js
+++ backend/controlers/userControler.js
@@ -1,5 +1,7 @@
 import asyncHandler from 'express-async-handler';
+
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc    Auth user /set token
 // route    POST /api/users/auth
@@ -30,6 +32,7 @@ export const registerUser = asyncHandler(async (req, res) => {
 	});
 
 	if (user) {
+		generateToken(res, user._id);
 		res.status(201).json({
 			_id: user._id,
 			name: user.name,
@@ -40,7 +43,7 @@ export const registerUser = asyncHandler(async (req, res) => {
 		throw new Error('Invalid user data');
 	}
 
-	res.status(200).json({ message: 'User registered!!!!' });
+	// res.status(200).json({ message: 'User registered!!!!' });
 });
 
 // @desc    Logout user
