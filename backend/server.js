9f5dd7e557412db64d1cff6b3a27b6adfc27d1ab
Jakub Karasek <jkarasek@Jakubs-MacBook-Air.local>
2023-07-31 23:43:27 +0200

    add protected routes handling

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -15,6 +16,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
