edd4738e94756faa7fe7140fe3d5ad2a434fdb3c
Jakub Karasek <jkarasek@Jakubs-MacBook-Air.local>
2023-09-18 00:54:21 +0200

    fix/remove duplicate code from getUserProfile

--- backend/controlers/userControler.js
+++ backend/controlers/userControler.js
@@ -77,13 +77,6 @@ export const getUserProfile = asyncHandler(async (req, res) => {
 	};
 
 	res.status(200).json(user);
-	const user = {
-		_id: req.user._id,
-		name: req.user.name,
-		email: req.user.email,
-	};
-
-	res.status(200).json(user);
 });
 
 // @desc    Update user profile
