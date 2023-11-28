3c48ffd0b1c900bd094ae9321b1027db2ca848a3
Jakub Karasek <jkarasek@Jakubs-MacBook-Air.local>
2023-11-28 21:43:46 +0100

    scaffold login ui and add redux

--- frontend/src/components/FormContainer/FormContainer.tsx
+++ frontend/src/components/FormContainer/FormContainer.tsx
@@ -2,7 +2,7 @@ import { FormContainerProps } from './FormContainer.types';
 
 const FormContainer = ({ children }: FormContainerProps) => {
 	return (
-		<div className="bg-white bg-opacity-10 border border-gray-200 rounded-lg py-4 px-10 border-opacity-25 text-gray-200 w-6/12 h-96 flex justify-center">
+		<div className="bg-white bg-opacity-5 border border-gray-200 rounded-lg py-4 px-10 border-opacity-25 text-gray-200 w-6/12 h-96 flex flex-col justify-start items-center gap-3">
 			{children}
 		</div>
 	);
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,7 +5,7 @@ const Login = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 
-	const submitHandler = async (e) => {
+	const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		console.log('Form submitted!');
 	};
@@ -13,7 +13,40 @@ const Login = () => {
 	return (
 		<main className="flex min-h-screen flex-col items-center p-24 gap-16">
 			<FormContainer>
-				<div className="">Login</div>
+				<h1 className="">Login</h1>
+				<form
+					className="flex flex-col justify-start items-center gap-3"
+					onSubmit={submitHandler}
+				>
+					{/* add label */}
+					<input
+						className="text-black"
+						type="email"
+						placeholder="email"
+						value={email}
+						onChange={(e) => {
+							setEmail(e.target.value);
+							console.log(e.target.value);
+						}}
+					/>
+					{/* add label */}
+					<input
+						className="text-black"
+						type="password"
+						placeholder="password"
+						value={password}
+						onChange={(e) => {
+							setPassword(e.target.value);
+							console.log(e.target.value);
+						}}
+					/>
+					<button
+						className="bg-white bg-opacity-5 border border-gray-200 rounded-lg py-1 px-5 border-opacity-25 hover:bg-opacity-20 ease-in-out duration-150"
+						type="submit"
+					>
+						Submit
+					</button>
+				</form>
 			</FormContainer>
 		</main>
 	);
